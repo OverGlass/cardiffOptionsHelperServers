47a03072f6cdd06ec263b7075cc6761a0e4ed3d5
Antonin Carlin <antonin.carlin@icloud.com>
2020-10-26 11:16:59 +0100

    feat: add env CLIENT_PATH

--- staticServerMiddleware.ts
+++ staticServerMiddleware.ts
@@ -1,8 +1,11 @@
 import { send, Context } from "./deps.ts";
+import { config } from "https://deno.land/x/dotenv@v0.5.0/mod.ts";
+
+const env = config();
 
 export default async function (context: Context) {
   await send(context, context.request.url.pathname, {
-    root: `${Deno.cwd()}/../webUI/`,
+    root: env.CLIENT_PATH,
     index: "index.html",
   });
 }
