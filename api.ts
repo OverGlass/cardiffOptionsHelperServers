a6c001fe6c4fc2b9c0a26f18374fa76681060d93
Antonin Carlin <antonin.carlin@icloud.com>
2021-06-30 09:38:38 +0200

    fix test

--- api.ts
+++ api.ts
@@ -1,7 +1,11 @@
 import { Application, Router } from "./deps.ts";
 import { oakCors } from "https://deno.land/x/cors/mod.ts";
 import asyncLogicAutoStock from "./asyncLogicAutoStock.ts";
-import { $cardiffFormatter, $getLogicAutoTextsbyRef, $getLogicAutobySearch } from "./apiMiddleware.ts";
+import {
+  $cardiffFormatter,
+  $getLogicAutobySearch,
+  $getLogicAutoTextsbyRef,
+} from "./apiMiddleware.ts";
 
 try {
   await asyncLogicAutoStock();
@@ -9,31 +13,36 @@ try {
   console.log(e);
 }
 
-const api = new Application();
-const controller = new AbortController();
-const { signal } = controller;
-const router = new Router();
+async function launchServer () {
+  const api = new Application();
+  const controller = new AbortController();
+  const { signal } = controller;
+  const router = new Router();
 
-api.addEventListener("error", evt => {
-  // Will log the thrown error to the console.
-  console.log("Event Error : \n", evt.error);
-  controller.abort();
-});
+  api.addEventListener("error", async (evt) => {
+    // Will log the thrown error to the console.
+    console.log("Event Error : \n", evt.error);
+    controller.abort();
+    await launchServer();
+  });
 
-router
-  .post("/api/cardiffFormat", $cardiffFormatter)
-  .post("/api/getLogicAutoTextsByRef", $getLogicAutoTextsbyRef)
-  .post("/api/getLogicAutobySearch", $getLogicAutobySearch);
+  router
+    .post("/api/cardiffFormat", $cardiffFormatter)
+    .post("/api/getLogicAutoTextsByRef", $getLogicAutoTextsbyRef)
+    .post("/api/getLogicAutobySearch", $getLogicAutobySearch);
 
-api.use(oakCors());
-api.use(router.routes());
-api.use(router.allowedMethods());
+  api.use(oakCors());
+  api.use(router.routes());
+  api.use(router.allowedMethods());
+  console.log("\nStart API on 8080");
+  await api.listen({ port: 8080, signal })
+  controller.abort()
+  await launchServer()
+}
 
-while (true) {
+
   try {
-    console.log("\nStart API on 8080");
-    await api.listen({ port: 8080, signal });
+    launchServer()
   } catch (e) {
     console.log("\nListener Error :\n -> ", e);
   }
-}
