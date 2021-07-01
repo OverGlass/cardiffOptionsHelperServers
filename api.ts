00b90e492f8f9bc8a1fdee98f74bddfb8082a3d4
Antonin Carlin <antonin.carlin@icloud.com>
2021-07-01 08:38:47 +0200

    test fix

--- api.ts
+++ api.ts
@@ -13,13 +13,13 @@ try {
   console.log(e);
 }
 
-async function launchServer () {
+async function launchServer() {
   const api = new Application();
   const controller = new AbortController();
   const { signal } = controller;
   const router = new Router();
 
-  api.addEventListener("error", async (evt) => {
+  api.addEventListener("error", async evt => {
     // Will log the thrown error to the console.
     console.log("Event Error : \n", evt.error);
     controller.abort();
@@ -28,21 +28,26 @@ async function launchServer () {
 
   router
     .post("/api/cardiffFormat", $cardiffFormatter)
-    .post("/api/getLogicAutoTextsByRef", $getLogicAutoTextsbyRef)
-    .post("/api/getLogicAutobySearch", $getLogicAutobySearch);
+    .post(
+      "/api/getLogicAutoTextsByRef",
+      $getLogicAutoTextsbyRef
+    )
+    .post(
+      "/api/getLogicAutobySearch",
+      $getLogicAutobySearch
+    );
 
   api.use(oakCors());
   api.use(router.routes());
   api.use(router.allowedMethods());
   console.log("\nStart API on 8080");
-  await api.listen({ port: 8080, signal })
-  controller.abort()
-  await launchServer()
+  await api.listen({ port: 8080, signal });
+  controller.abort();
+  await launchServer();
 }
 
-
-  try {
-    launchServer()
-  } catch (e) {
-    console.log("\nListener Error :\n -> ", e);
-  }
+try {
+  await launchServer();
+} catch (e) {
+  console.log("\nListener Error :\n -> ", e);
+}
